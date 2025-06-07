5c743c4b7ac9c9cac0887eb6189119a0676b3e33
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-07 14:15:27 +0530

    my profile:

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ✅ use HashRouter
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );

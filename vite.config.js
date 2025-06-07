3ccbdacaee604d8049060a0e73626cce53f89f04
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-07 14:04:05 +0530

    my profile:

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/profile/", // <-- Replace 'profile' with your actual GitHub repo name
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
